81e5e631962a6a6af8e8228bf500fef9a5bf7a20
高谚宾 <gaoyanbin@agrithings.cn>
2018-08-31 10:40:29 +0800

    2018-08-31

--- 04_SprintBootRestFull/spring-boot-rest2/src/main/java/com/blueice/demo/rest/controller/UserController.java
+++ 04_SprintBootRestFull/spring-boot-rest2/src/main/java/com/blueice/demo/rest/controller/UserController.java
@@ -3,6 +3,8 @@ package com.blueice.demo.rest.controller;
 import com.blueice.demo.rest.bean.User;
 import com.blueice.demo.rest.dao.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,9 +19,20 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
+
     @GetMapping
-    public List<User> getUsers(){
+    public String getUser() {
+        return "Hello Spring Security";
+    }
+
+    @GetMapping("list")
+    public List<User> getUserList(){
         return userRepository.listUser();
+    }
+
+    @GetMapping("hello")
+    public String getHello(){
+        return "Hello";
     }
 
     @GetMapping("{id}")
---
+++ 05_SprintBootSecurity/spring-boot-security-demo01/src/main/java/com/blueice/springboot/security/bean/User.java
@@ -0,0 +1,54 @@
+package com.blueice.springboot.security.bean;
+
+/**
+ * Created by Administrator on 2018/8/22.
+ */
+
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+
+    //必须要有
+    public User(){
+
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
