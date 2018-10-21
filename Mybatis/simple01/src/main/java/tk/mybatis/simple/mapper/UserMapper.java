0b0e3465e04049cf38dce834f817858c92fe082b
Gaoyanbin <384401056@qq.com>
2018-10-22 03:29:41 +0800

    mybatis-foreach

--- Mybatis/simple01/src/main/java/tk/mybatis/simple/mapper/UserMapper.java
+++ Mybatis/simple01/src/main/java/tk/mybatis/simple/mapper/UserMapper.java
@@ -4,7 +4,9 @@ import org.apache.ibatis.annotations.Param;
 import tk.mybatis.simple.model.Role;
 import tk.mybatis.simple.model.User;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 public interface UserMapper {
 
@@ -31,4 +33,14 @@ public interface UserMapper {
 
     //根据两个条件来查询，两个条件都有可能为空，如果都为空不返回结果
     List<User> selectByUserIdOrUserName(@Param("userId")Long userId, @Param("userName")String userName);
+
+    //当参数是list类型时，可以用@Param指定别名，这样在xml中可以使用。
+    List<User> selectByIdList(@Param("myList")List<Long> ids);
+
+    //批量插入数据
+    int insertListUser(@Param("userList")List<User> userList);
+
+    //根据Map参数进行动态更新数据，当用map作为参数时,在xml中的collection属性就要赋值为:_parameter.
+    //这里指定别名，并在xml中的collection属性中使用，会报错
+    int updateByMap(Map<String, Object> userMap);
 }
--- Mybatis/simple01/src/test/java/test/UserTest/UserMapperTest.java
+++ Mybatis/simple01/src/test/java/test/UserTest/UserMapperTest.java
@@ -1,48 +1,53 @@
 package test.UserTest;
 
+import com.google.common.collect.Lists;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.session.SqlSession;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.collections.Maps;
 import test.BaseMapperTest;
 import tk.mybatis.simple.mapper.UserMapper;
 import tk.mybatis.simple.model.Role;
 import tk.mybatis.simple.model.User;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserMapperTest extends BaseMapperTest {
 
     @Test
-    public void testSelectById(){
+    public void testSelectById() {
         SqlSession sqlSession = getSqlsession();
         try {
             UserMapper mapper = sqlSession.getMapper(UserMapper.class);
             User user = mapper.selectById(1L);
-            System.out.printf("User:"+user);
+            System.out.printf("User:" + user);
             Assert.assertEquals("admin", user.getUserName());
-        }finally {
+        } finally {
             sqlSession.close();
         }
     }
 
     @Test
-    public void testSelectRoles(){
+    public void testSelectRoles() {
         SqlSession sqlSession = getSqlsession();
         try {
             UserMapper mapper = sqlSession.getMapper(UserMapper.class);
             List<Role> roles = mapper.selectRolesByUserId(1L);
-            System.out.printf("RolesSize:"+roles.size()+"\n");
-            for(Role item: roles){
-                System.out.printf(item.toString()+"\n");
+            System.out.printf("RolesSize:" + roles.size() + "\n");
+            for (Role item : roles) {
+                System.out.printf(item.toString() + "\n");
             }
-        }finally {
+        } finally {
             sqlSession.close();
         }
     }
 
     @Test
-    public void testInsert(){
+    public void testInsert() {
         SqlSession sqlSession = getSqlsession();
         try {
             UserMapper mapper = sqlSession.getMapper(UserMapper.class);
@@ -51,14 +56,14 @@ public class UserMapperTest extends BaseMapperTest {
             user.setUserPassword("123456");
             user.setUserEmail("lili@163.com");
             user.setUserInfo("fdsafdksalfjdklsafj;d");
-            user.setHeadImg(new byte[]{1,2,3});
+            user.setHeadImg(new byte[]{1, 2, 3});
             user.setCreateTime(new Date());
 
             int result = mapper.insert(user);
 
             Assert.assertEquals(1, result);
 
-        }finally {
+        } finally {
 //            sqlSession.commit();//提交数据
             sqlSession.rollback();//为了不影响数据，这里直接回滚.
             sqlSession.close();
@@ -67,7 +72,7 @@ public class UserMapperTest extends BaseMapperTest {
 
 
     @Test
-    public void testInsertAndGetId(){
+    public void testInsertAndGetId() {
         SqlSession sqlSession = getSqlsession();
         try {
             UserMapper mapper = sqlSession.getMapper(UserMapper.class);
@@ -76,13 +81,13 @@ public class UserMapperTest extends BaseMapperTest {
             user.setUserPassword("123456");
             user.setUserEmail("Jims@163.com");
             user.setUserInfo("我是jims,哈哈哈...");
-            user.setHeadImg(new byte[]{1,2,3,4,5,6,7,8});
+            user.setHeadImg(new byte[]{1, 2, 3, 4, 5, 6, 7, 8});
             user.setCreateTime(new Date());
             int result = mapper.insertAndGetId(user);
             Assert.assertEquals(1, result);
             Assert.assertNotNull(user.getId());
             System.out.printf("userId:" + user.getId());
-        }finally {
+        } finally {
 //            sqlSession.commit();//提交数据
             sqlSession.rollback();//为了不影响数据，这里直接回滚.
             sqlSession.close();
@@ -90,7 +95,7 @@ public class UserMapperTest extends BaseMapperTest {
     }
 
     @Test
-    public void testInsertAndGetId2(){
+    public void testInsertAndGetId2() {
         SqlSession sqlSession = getSqlsession();
         try {
             UserMapper mapper = sqlSession.getMapper(UserMapper.class);
@@ -99,13 +104,13 @@ public class UserMapperTest extends BaseMapperTest {
             user.setUserPassword("123456");
             user.setUserEmail("Jims@163.com");
             user.setUserInfo("我是jims,哈哈哈...");
-            user.setHeadImg(new byte[]{1,2,3,4,5,6,7,8});
+            user.setHeadImg(new byte[]{1, 2, 3, 4, 5, 6, 7, 8});
             user.setCreateTime(new Date());
             int result = mapper.insertAndGetId2(user);
             Assert.assertEquals(1, result);
             Assert.assertNotNull(user.getId());
             System.out.printf("userId:" + user.getId());
-        }finally {
+        } finally {
 //            sqlSession.commit();//提交数据
             sqlSession.rollback();//为了不影响数据，这里直接回滚.
             sqlSession.close();
@@ -113,7 +118,7 @@ public class UserMapperTest extends BaseMapperTest {
     }
 
     @Test
-    public void update(){
+    public void update() {
         SqlSession sqlSession = getSqlsession();
         try {
             UserMapper mapper = sqlSession.getMapper(UserMapper.class);
@@ -122,13 +127,13 @@ public class UserMapperTest extends BaseMapperTest {
             user.setUserPassword("123456");
             user.setUserEmail("Gaoyanbin@163.com");
             user.setUserInfo("我是高谚宾,哈哈哈...");
-            user.setHeadImg(new byte[]{1,2});
+            user.setHeadImg(new byte[]{1, 2});
             user.setCreateTime(new Date());
             int result = mapper.update(user);
             Assert.assertEquals(1, result);
             Assert.assertEquals("高谚宾", user.getUserName());
-            System.out.printf("User:"+user.toString());
-        }finally {
+            System.out.printf("User:" + user.toString());
+        } finally {
             //sqlSession.commit();//提交数据
             sqlSession.rollback();//为了不影响数据，这里直接回滚.
             sqlSession.close();
@@ -137,7 +142,7 @@ public class UserMapperTest extends BaseMapperTest {
 
 
     @Test
-    public void delete(){
+    public void delete() {
         SqlSession sqlSession = getSqlsession();
         try {
             UserMapper mapper = sqlSession.getMapper(UserMapper.class);
@@ -146,7 +151,7 @@ public class UserMapperTest extends BaseMapperTest {
             int result = mapper.delete(1037L);//删除用户记录
             Assert.assertEquals(1, result);
             Assert.assertNull(mapper.selectById(1037L));
-        }finally {
+        } finally {
             //sqlSession.commit();//提交数据
             sqlSession.rollback();//为了不影响数据，这里直接回滚.
             sqlSession.close();
@@ -154,31 +159,89 @@ public class UserMapperTest extends BaseMapperTest {
     }
 
     @Test
-    public void selectRolesByUserIdAndRoleEnabled(){
+    public void selectRolesByUserIdAndRoleEnabled() {
         SqlSession sqlSession = getSqlsession();
         try {
             UserMapper mapper = sqlSession.getMapper(UserMapper.class);
-            List<Role> roleList = mapper.selectRolesByUserIdAndRoleEnabled(1L,1);
-            System.out.printf("roleListSize:"+roleList.size());
-            Assert.assertTrue(roleList.size()>0);
-        }finally {
+            List<Role> roleList = mapper.selectRolesByUserIdAndRoleEnabled(1L, 1);
+            System.out.printf("roleListSize:" + roleList.size());
+            Assert.assertTrue(roleList.size() > 0);
+        } finally {
             sqlSession.close();
         }
     }
+
 
     @Test
     public void selectByUserIdOrUserName() {
         SqlSession sqlSession = getSqlsession();
         try {
             UserMapper mapper = sqlSession.getMapper(UserMapper.class);
-//            List<User> userList = mapper.selectByUserIdOrUserName(null, null);//无返回
-//            List<User> userList = mapper.selectByUserIdOrUserName(null, "Jims");//只通过userName进行查询
-//            List<User> userList = mapper.selectByUserIdOrUserName(1036L, "");//只通过userId进行查询
-            List<User> userList = mapper.selectByUserIdOrUserName(1L, "admin");//userId and userName
+//            List<User> userList = mapper.selectByUserIdOrUserName(null ,null);
+//            List<User> userList = mapper.selectByUserIdOrUserName(1 ,null);
+//            List<User> userList = mapper.selectByUserIdOrUserName(null ,"admin");
+            List<User> userList = mapper.selectByUserIdOrUserName(1001L, "test");
 
         } finally {
             sqlSession.close();
         }
     }
+
+
+    @Test
+    public void selectByIdList() {
+        SqlSession sqlSession = getSqlsession();
+        try {
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            List<User> userList = mapper.selectByIdList(Lists.newArrayList(1L, 1001L));
+            Assert.assertTrue(userList.size() > 0);
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void insertListUser() {
+        SqlSession sqlSession = getSqlsession();
+        try {
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            List<User> userList = Lists.newArrayList();
+            for (int i = 0; i < 10; i++) {
+                User user = new User();
+                user.setUserName("Test" + i);
+                user.setUserPassword("123456");
+                user.setUserEmail("test@163.com");
+                user.setUserInfo("test" + i);
+                user.setHeadImg(new byte[]{1, 2, 3});
+                user.setCreateTime(new Date());
+                userList.add(user);
+            }
+            int result = mapper.insertListUser(userList);
+            System.out.println("result:" + result);
+            Assert.assertEquals(10, result);
+        } finally {
+            sqlSession.rollback();
+            sqlSession.close();
+        }
+    }
+
+
+    @Test
+    public void updateByMap() {
+        SqlSession sqlSession = getSqlsession();
+        try {
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            Map<String, Object> userMap = new HashMap<String, Object>();
+            userMap.put("id", 1001L);
+            userMap.put("user_info", "神经病");
+            userMap.put("user_email", "sjb@11133344.com");
+            int reslut = mapper.updateByMap(userMap);
+            Assert.assertEquals(1, reslut);
+        } finally {
+            sqlSession.rollback();
+            sqlSession.close();
+        }
+    }
+
 
 }
